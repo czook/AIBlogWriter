b5a69b01f48cd1bb801641f845dc891eaea0ceeb
czook <czook@iastate.edu>
2023-05-08 15:55:52 -0400

    added outline and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+### AI Niche Blog Writer
+
+The goal of this project is to code a project that takes keywords give by user that are in a csv or a google sheets page and turns them into multiple blog posts.
+
+using OpenAI api
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
 fn main() {
     println!("Hello, world!");
+    //keywords from csv or google docs
+    //
+    //create content hubs / silos
+    //
+    //create title, headings, meta description,and slug
+    //
+    //create featured images
+    //
+    //create blog content
+    //
+    //internal links
+    //
+    //upload to Wordpress
 }
